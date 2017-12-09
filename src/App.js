de959be3c52ece9c19ee8d78a39a7f6f0b55b095
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-12-09 12:42:36 -0500

    Setup film and details sections

--- src/App.js
+++ src/App.js
@@ -5,15 +5,17 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <main>
+        <div className="film-library">
+          <div clasName="film-list">
+            <h1 className="section-title">FILMS</h1>
+          </div>
+        </div>
+
+        <div className="film-details">
+          <h1 className="section-title">DETAILS</h1>
+        </div>
+      </main>
     );
   }
 }
