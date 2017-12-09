50ab779a9dd003ff2d1e87588f3f2afa22a56e14
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-12-09 12:47:49 -0500

    Move film ist to component

---
+++ src/FilmList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class FilmList extends Component {
+  render() {
+    return (
+      <section clasName="film-list">
+        <h1 className="section-title">FILMS</h1>
+      </section>
+    );
+  }
+}
+
+export default FilmList;
