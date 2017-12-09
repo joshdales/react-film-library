4b909456a97c4c0aa1d224b37ce591d59300c620
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-12-09 16:53:25 -0500

    Add filters to the top of film listing

--- src/FilmListing.js
+++ src/FilmListing.js
@@ -2,18 +2,26 @@ import React, { Component } from 'react';
 import FilmRow from './FilmRow';
 
 class FilmList extends Component {
-  constructor(props){
-    super(props)
-  }
 
   handleFaveClick(filter){
     console.log(`Setting filter to [${filter}]`);
   }
-  
+
   render() {
     return (
       <section className="film-list">
         <h1 className="section-title">FILMS</h1>
+        <div className="film-list-filters">
+          <div className="film-list-filter">
+            ALL
+            <span className="section-count">{this.props.films.length}</span>
+          </div>
+          <div className="film-list-filter">
+            FAVES
+            <span className="section-count">0</span>
+          </div>
+        </div>
+
         {this.props.films.map(film =>
           <FilmRow key={film.id} film={film}/>
         )}
