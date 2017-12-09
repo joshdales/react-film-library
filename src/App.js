ba32ea3ed25988fcb83c79c84762379745cb2203
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-12-09 13:22:14 -0500

    Import films as props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import FilmList from './FilmList';
 import FilmDetails from './FilmDetails';
+import TMDB from './TMDB';
 
 class App extends Component {
   render() {
+    this.props = TMDB.films
     return (
       <main className="film-library">
           <FilmList/>
