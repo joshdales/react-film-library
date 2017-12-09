769fa1bbc1a5b59f78668edaa2fb20e3bc896949
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-12-09 12:46:15 -0500

    Move film-libary to <main>and remove app.css and react logo

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <main>
-        <div className="film-library">
+      <main className="film-library">
           <div clasName="film-list">
             <h1 className="section-title">FILMS</h1>
           </div>
-        </div>
 
-        <div className="film-details">
-          <h1 className="section-title">DETAILS</h1>
-        </div>
+          <div className="film-details">
+            <h1 className="section-title">DETAILS</h1>
+          </div>
       </main>
     );
   }
