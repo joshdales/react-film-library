8a10033a28e1d664ac6bcdfb0a8dfd661599264d
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-12-10 15:40:19 -0500

    Filter click will change class of film-list-filters

--- src/FilmListing.js
+++ src/FilmListing.js
@@ -5,22 +5,27 @@ class FilmList extends Component {
   constructor(props){
     super(props);
     this.handleFilterClick = this.handleFilterClick.bind(this);
+    this.state = {
+      filter: 'all'
+    }
   }
 
   handleFilterClick(filter){
     console.log(`Setting filter to [${filter}]`);
+    this.setState({filter: filter})
   }
 
   render() {
+    const {filter} = this.state
     return (
       <section className="film-list">
         <h1 className="section-title">FILMS</h1>
         <div className="film-list-filters">
-          <div className="film-list-filter" onClick={() => this.handleFilterClick('all')}>
+          <div className={`film-list-filter ${ filter === 'all' ? 'is-active' : '' }`} onClick={() => this.handleFilterClick('all')}>
             ALL
             <span className="section-count">{this.props.films.length}</span>
           </div>
-          <div className="film-list-filter" onClick={() => this.handleFilterClick('fave')}>
+          <div className={`film-list-filter ${ filter === 'filter' ? 'is-active' : '' }`} onClick={() => this.handleFilterClick('fave')}>
             FAVES
             <span className="section-count">0</span>
           </div>
