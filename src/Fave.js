205f05f5cf718c2d8d6f4d4d3c438c69aea92f08
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-12-10 14:59:46 -0500

    Set Fave class to change with state (i think)

--- src/Fave.js
+++ src/Fave.js
@@ -4,16 +4,20 @@ class Fave extends Component {
   constructor(props){
     super(props);
     this.handleClick = this.handleClick.bind(this)
+    this.state = {
+      isFave: false
+    }
   }
 
   handleClick(e){
     e.stopPropagation();
     console.log("handling click");
-  }
+  };
 
   render() {
+    const {isFave} = this.state;
     return (
-      <div className="film-row-fave add_to_queue" onClick={this.handleClick}>
+      <div className={`film-row-fave ${isFave ? 'add_to_queue': 'remove_from_queue'}`} onClick={this.handleClick}>
         <i className="material-icons">add_to_queue</i>
       </div>
     );
