2c79ca877749b1b048b087dde798c058a7f0b065
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-12-10 15:47:56 -0500

    Add constructor to App

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import FilmDetails from './FilmDetails';
 import TMDB from './TMDB';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+  }
   render() {
     this.props = TMDB.films
     return (
