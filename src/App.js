582722e859abf1afdf26a203b6d15859c2bc7205
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-12-11 17:35:39 -0500

    Fetch response from url

--- src/App.js
+++ src/App.js
@@ -35,7 +35,13 @@ class App extends Component {
 
   handleDetailsToggle(film){
     console.log(`Fetching details for ${film.title}`);
-    this.setState({current: film})
+    this.setState({current: film});
+    const url = `https://api.themoviedb.org/3/movie/${film.id}?api_key=${TMDB.api_key}&append_to_response=videos,images&language=en`
+    fetch(url).then(response => {
+      response.json().then(data => {
+        console.log(data);
+      })
+    })
   }
 
   render() {
--- src/TMDB.js
+++ src/TMDB.js
@@ -1,4 +1,5 @@
 import dotenv from 'dotenv';
+dotenv.config();
 
 const TMDB = {
   api_key: process.env.REACT_APP_TMDB_API_KEY,
