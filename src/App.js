0328467ab72e29252dec747845c7b8372f2b3e8c
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-12-11 18:03:00 -0500

    Refactor FilmPoster

--- src/App.js
+++ src/App.js
@@ -16,11 +16,8 @@ class App extends Component {
   }
 
   handleFaveToggle(film) {
-    console.log(film);
     const faves = this.state.faves.slice();
-    console.log(faves);
     const filmIndex = faves.indexOf(film);
-    console.log(filmIndex);
 
     if (filmIndex > -1) {
       console.log(`Removing ${film.title} from FAVES`);
@@ -35,11 +32,11 @@ class App extends Component {
 
   handleDetailsToggle(film){
     console.log(`Fetching details for ${film.title}`);
-    this.setState({current: film});
     const url = `https://api.themoviedb.org/3/movie/${film.id}?api_key=${TMDB.api_key}&append_to_response=videos,images&language=en`
     fetch(url).then(response => {
       response.json().then(data => {
         console.log(data);
+        this.setState({current: data})
       })
     })
   }
--- src/FilmPoster.js
+++ src/FilmPoster.js
@@ -1,13 +1,10 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class FilmPoster extends Component {
-  render() {
-    return (
-      <figure className="film-poster">
-        <img src={`https://image.tmdb.org/t/p/w780/${this.props.film.poster_path}`} alt=""/>
-      </figure>
-    );
-  }
-}
+function FilmPoster(props) {
+  return (
+  <figure className="film-poster">
+    <img src={`https://image.tmdb.org/t/p/w780/${props.film.poster_path}`} alt=""/>
+  </figure>
+  )};
 
 export default FilmPoster;
