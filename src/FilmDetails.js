0473fa3804d35c4ee0e6dcda43774cdf8b3ec648
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-12-11 18:25:58 -0500

    Display no film selected on FilmDetails

--- src/FilmDetails.js
+++ src/FilmDetails.js
@@ -2,9 +2,20 @@ import React, { Component } from 'react';
 
 class FilmDetails extends Component {
   render() {
+    // const backdropUrl = `https://image.tmdb.org/t/p/w1280/${props.film.current.backdrop_path}`
+    // const posterUrl = `https://image.tmdb.org/t/p/w780/${props.film.current.poster_path}`
+
     return (
       <section className="film-details">
         <h1 className="section-title">DETAILS</h1>
+
+        <div className="film-detail">
+          <p>
+            <i className='material-icons'>subscriptions</i>
+            <span>No film selected</span>
+          </p>
+
+        </div>
       </section>
     );
   }
