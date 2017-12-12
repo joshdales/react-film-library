6c2093e3254eb4522336f62ffcfed970417f993b
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-12-12 09:24:03 -0500

    Show current if any is selected

--- src/FilmDetails.js
+++ src/FilmDetails.js
@@ -1,25 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class FilmDetails extends Component {
-  render() {
-    let {details} = this.props
-    // const backdropUrl = `https://image.tmdb.org/t/p/w1280/${details.current.backdrop_path}`
-    // const posterUrl = `https://image.tmdb.org/t/p/w780/${details.current.poster_path}`
+function FilmDetails(props) {
+  // const posterUrl = `https://image.tmdb.org/t/p/w780/${props.current.poster_path}`;
+  let detail;
 
-    return (
-      <section className="film-details">
-        <h1 className="section-title">DETAILS</h1>
+  if (props.current.id) { detail = (
+    <div className="is-hydrated">
+      <figure className="film-backdrop">
+        <img src={`https://image.tmdb.org/t/p/w1280/${props.current.backdrop_path}`} alt=""/>
+        <figcaption className="film-title">{props.current.title}</figcaption>
+      </figure>
+    </div>
+  )} else { detail =(
+    <div className="film-detail">
+      <p>
+        <i className='material-icons'>subscriptions</i>
+        <span>No film selected</span>
+      </p>
+    </div>
+  )}
 
-        <div className="film-detail">
-          <p>
-            <i className='material-icons'>subscriptions</i>
-            <span>No film selected</span>
-          </p>
-
-        </div>
-      </section>
-    );
-  }
-}
+  return (
+    <section className="film-details">
+      <h1 className="section-title">DETAILS</h1>
+      {detail}
+    </section>
+  );
+};
 
 export default FilmDetails;
