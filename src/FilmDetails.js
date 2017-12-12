86a1abe43c932a40db9f108f2db7e19fbe75ba25
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-12-12 10:07:33 -0500

    Display all relevant film information in FilmDetails

--- src/FilmDetails.js
+++ src/FilmDetails.js
@@ -10,6 +10,14 @@ function FilmDetails(props) {
         <img src={`https://image.tmdb.org/t/p/w1280/${props.current.backdrop_path}`} alt=""/>
         <figcaption className="film-title">{props.current.title}</figcaption>
       </figure>
+
+      <div className="film-meta">
+        <h2 className="film-tagline">{props.current.tagline}</h2>
+        <p className="film-detail-overview">
+          <img src={`https://image.tmdb.org/t/p/w780/${props.current.poster_path}`} className="film-detail-poster" alt={props.current.title}/>
+          {props.current.overview}
+        </p>
+      </div>
     </div>
   )} else { detail = (
     <div className="film-detail">
